cccfcbeea2ef794ed8d3c3133fdbabeceb8481fc
Wojagg <adam.wojakowski.biz@gmail.com>
2024-07-30 19:38:53 +0200

    updated configuration and modules in music-service package

--- packages/music-service/src/app.module.ts
+++ packages/music-service/src/app.module.ts
@@ -1,27 +1,45 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
-import { AppController } from './Controller/app.controller';
-import { AppService } from './Service/app.service';
-
+import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { User } from './users/user.entity';
-
 import { config } from './config/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { GraphQLVoid } from 'graphql-scalars';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mongodb',
-      host: config.mongo.host,
-      port: config.mongo.port,
-      username: config.mongo.user,
-      password: config.mongo.password,
-      database: config.mongo.db,
+      url: `mongodb://${config.mongo.user}:${config.mongo.password}@${config.mongo.host}:${config.mongo.port}`,
       entities: [User],
       synchronize: false,
+      database: config.mongo.database,
+      authSource: config.mongo.authSource,
+    }),
+    RedisModule.forRoot({
+      config: {
+        host: config.redis.host,
+        port: config.redis.port,
+        username: config.redis.user,
+        password: config.redis.password,
+      },
+    }),
+    UsersModule,
+    AuthModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+      resolvers: { Void: GraphQLVoid },
+      includeStacktraceInErrorResponses: false,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
